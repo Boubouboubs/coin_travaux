5ee08b1a4aaf4361d240a894681a36915a904ca4
d.s <serrano.david.liaca@gmail.com>
2023-06-06 18:06:39 +0200

    almost finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  has_many :projects
+  has_many :projects, dependent: :destroy
   has_many :project_companies, through: :projects
   belongs_to :company, optional: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :photo
 end
