38d0fc99dbdab2540f9c8944ca6b0cfe7cd5c8d0
d.s <serrano.david.liaca@gmail.com>
2023-06-06 17:46:30 +0200

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,56 +11,51 @@ db_main_projects = Rails.root.join("db", "DB_main_projects.csv")
 puts "Creation of users..."
 users = []
 camille = User.create(email: "camille.dion@gmail.com", password: "123456", first_name: "Camille", last_name: "Dion", phone_number: "0611223344")
-users << david = User.create!(email: "serrano.david.liaca@gmail.com", password: "123456", first_name: "David", last_name: "Serrano", phone_number: "0611223366")
-users << amal = User.create!(email: "amal.rabbani@gmail.com", password: "123456", first_name: "Amal", last_name: "Rabbani", phone_number: "0611223377")
-users << boubou = User.create!(email: "boubouboubs@gmail.com", password: "123456", first_name: "Stéphanie", last_name: "Boubou", phone_number: "0611223388")
-users << celine = User.create!(email: "celine.chader@gmail.com", password: "123456", first_name: "Céline", last_name: "Chader", phone_number: "0611223399")
-puts "#{users.size} users created!"
+# users << david = User.create!(email: "serrano.david.liaca@gmail.com", password: "123456", first_name: "David", last_name: "Serrano", phone_number: "0611223366")
+# users << amal = User.create!(email: "amal.rabbani@gmail.com", password: "123456", first_name: "Amal", last_name: "Rabbani", phone_number: "0611223377")
+# users << boubou = User.create!(email: "boubouboubs@gmail.com", password: "123456", first_name: "Stéphanie", last_name: "Boubou", phone_number: "0611223388")
+# users << celine = User.create!(email: "celine.chader@gmail.com", password: "123456", first_name: "Céline", last_name: "Chader", phone_number: "0611223399")
+# puts "#{users.size} users created!"
 
-camille.photo.attach(io: File.open('app/assets/images/camille.jpg'), filename: "#{camille.first_name}.jpg")
-celine.photo.attach(io: File.open('app/assets/images/celine.jpg'), filename: "#{celine.first_name}.jpg")
-boubou.photo.attach(io: File.open('app/assets/images/boubou.jpg'), filename: "#{boubou.first_name}.jpg")
-amal.photo.attach(io: File.open('app/assets/images/armelle.jpg'), filename: "#{amal.first_name}.jpg")
-david.photo.attach(io: File.open('app/assets/images/david.jpg'), filename: "#{david.first_name}.jpg")
+# camille.photo.attach(io: File.open('app/assets/images/camille.jpg'), filename: "#{camille.first_name}.jpg")
+# celine.photo.attach(io: File.open('app/assets/images/celine.jpg'), filename: "#{celine.first_name}.jpg")
+# boubou.photo.attach(io: File.open('app/assets/images/boubou.jpg'), filename: "#{boubou.first_name}.jpg")
+# amal.photo.attach(io: File.open('app/assets/images/armelle.jpg'), filename: "#{amal.first_name}.jpg")
+# david.photo.attach(io: File.open('app/assets/images/david.jpg'), filename: "#{david.first_name}.jpg")
 
-# Entrepreneurs entreprises
-puts "parsing CSV companies"
-CSV.foreach(filepath, headers: :first_row, col_sep: ";") do |row|
-  address = "#{row["Address"]}, #{row["Postcode"]}, #{row["City"]}"
-  company = Company.create!(name: row[0], address: address, creation_date: row["creation_date" ])
-  user = User.create!(email: row["Email"], company: company, password: "123456", phone_number: row["Phone_number"], first_name: row["First_name"], last_name: row["Last_name"])
-end
-puts "CSV parsed!!!"
+# # Entrepreneurs entreprises
+# puts "parsing CSV companies"
+# CSV.foreach(filepath, headers: :first_row, col_sep: ";") do |row|
+#   address = "#{row["Address"]}, #{row["Postcode"]}, #{row["City"]}"
+#   company = Company.create!(name: row[0], address: address, creation_date: row["creation_date" ])
+#   user = User.create!(email: row["Email"], company: company, password: "123456", phone_number: row["Phone_number"], first_name: row["First_name"], last_name: row["Last_name"])
+# end
+# puts "CSV parsed!!!"
 
-# Projets
-puts "creation of main_company_projects"
-CSV.foreach(db_main_projects, headers: :first_row, col_sep: ";") do |row|
-  address = "#{row["Address"]}, #{row["Postcode"]}, #{row["City"]}"
-  # user = User.create!(email: row["Email"], company: company, password: "123456", phone_number: row["Phone_number"], first_name: row["First_name"], last_name: row["Last_name"])
-  # company = Company.create!(name: row[0], address: address, creation_date: creation_date)
-  main_company_projects = Project.create!(name: row["City"], address: address, surface: row["Surface"], company: company, user: user)
-end
-
-main_company_projects.each do |project|
-  project = Project.create!(project)
-  project.user = users.sample
-  project.save!
-  project.companies << Company.first
-end
-
-# Projects
-puts "creation of projects"
-CSV.foreach(db_projects, headers: :first_row, col_sep: ";") do |row|
-  address = "#{row["Address"]}, #{row["Postcode"]}, #{row["City"]}"
-  # user = User.create!(email: row["Email"], company: company, password: "123456", phone_number: row["Phone_number"], first_name: row["First_name"], last_name: row["Last_name"])
-  # company = Company.create!(name: row[0], address: address, creation_date: creation_date)
-  projects = Project.create!(name: row["City"], address: address, surface: row["Surface"], company: company, user: user)
-end
+# # Projets
+# puts "creation of main_company_projects"
+# CSV.foreach(db_main_projects, headers: :first_row, col_sep: ";") do |row|
+#   address = "#{row["Address"]}, #{row["Postcode"]}, #{row["City"]}"
+#   # user = User.create!(email: row["Email"], company: company, password: "123456", phone_number: row["Phone_number"], first_name: row["First_name"], last_name: row["Last_name"])
+#   # company = Company.create!(name: row[0], address: address, creation_date: creation_date)
+#   main_company_project = Project.create!(address: address, surface: row["Surface"], user: user)
+#   ProjectCompany.create(project: main_company_project, company: company.first)
+# end
 
 
-projects.each do |project|
-  project = Project.create!(project)
-  project.user = users.sample
-  project.save!
-  project.companies << Company.where.not(id: Company.first.id).sample
-end
+# # # Projects
+# # puts "creation of projects"
+# # CSV.foreach(db_projects, headers: :first_row, col_sep: ";") do |row|
+# #   address = "#{row["Address"]}, #{row["Postcode"]}, #{row["City"]}"
+# #   # user = User.create!(email: row["Email"], company: company, password: "123456", phone_number: row["Phone_number"], first_name: row["First_name"], last_name: row["Last_name"])
+# #   # company = Company.create!(name: row[0], address: address, creation_date: creation_date)
+# #   projects = Project.create!(name: row["City"], address: address, surface: row["Surface"], company: company, user: user)
+# # end
+
+
+# # projects.each do |project|
+# #   project = Project.create!(project)
+# #   project.user = users.sample
+# #   project.save!
+# #   project.companies << Company.where.not(id: Company.first.id).sample
+# # end
