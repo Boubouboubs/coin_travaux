199c7d5679ac4f8aa1e78af5afb42a7deb5b0edb
d.s <serrano.david.liaca@gmail.com>
2023-06-07 09:02:39 +0200

    add description to companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_104409) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_065950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_104409) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "project_companies", force: :cascade do |t|
