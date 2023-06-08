b7d8f905fd395bf6884a311baf027c1af518900b
stephanie bourdillon <bourdillon.s@gmail.com>
2023-06-08 17:58:29 +0200

    simple form en cours

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,21 @@
 class CompaniesController < ApplicationController
   def index
     @companies = policy_scope(Company)
+    if params.dig(:filters, :star_rating).present?
+      @companies = @companies.select { |c| c.average_rating.round == params[:filters][:star_rating].to_i }
+    end
+    unless params.dig(:filters, :photo).in? [nil, '']
+      if params.dig(:filters, :photo) == "true"
+        @companies = @companies.select { |c| c.project_photo.present? }
+      end
+      if params.dig(:filters, :photo) == "false"
+        @companies = @companies.reject { |c| c.project_photo.present? }
+      end
+    end
     @user_project = Project.find(params[:project_id])
+
+
+
   end
 
   def show
