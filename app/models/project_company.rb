bfc286ac4a381ac8289d17f331fa5b9e8fb741bd
d.s <serrano.david.liaca@gmail.com>
2023-06-09 12:12:40 +0200

    services for demo + little fixes

--- app/models/project_company.rb
+++ app/models/project_company.rb
@@ -2,5 +2,5 @@ class ProjectCompany < ApplicationRecord
   belongs_to :project
   belongs_to :company
   has_many :reviews, dependent: :destroy
-  has_many :quotes, dependent: :destroy
+  has_one :quote, dependent: :destroy
 end
