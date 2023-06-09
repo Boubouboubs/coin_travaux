b6f89b6001dd540cb0626d6b1717fb33bf95410d
Amal Rabbani <130931565+AmalRabbani@users.noreply.github.com>
2023-06-09 11:47:32 +0200

    create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,17 @@ class ProjectsController < ApplicationController
     authorize @project
   end
 
+  def create
+    @project = Project.new(project_params)
+    authorize @project
+    @project.user = current_user
+    if @project.save
+      redirect_to project_companies_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def setup_video_call_token
@@ -26,6 +37,10 @@ class ProjectsController < ApplicationController
     twilio.generate_token(current_user, @user)
     @twilio_jwt = twilio.jwt
     @room_id = twilio.room_id
+  end
+
+  def project_params
+    params.require(:project).permit(:visit_date, :property_type, :address, :surface)
   end
 
 end
