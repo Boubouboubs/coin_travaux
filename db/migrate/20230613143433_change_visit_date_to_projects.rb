bb7b428a18fb07883a55438b96f086c090fba432
stephanie bourdillon <bourdillon.s@gmail.com>
2023-06-13 16:53:13 +0200

    change projects datetime

--- db/migrate/20230613143433_change_visit_date_to_projects.rb
+++ db/migrate/20230613143433_change_visit_date_to_projects.rb
@@ -1,4 +1,5 @@
 class ChangeVisitDateToProjects < ActiveRecord::Migration[7.0]
   def change
+    change_column :projects, :visit_date, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_091038) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_143433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_091038) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.date "visit_date"
+    t.datetime "visit_date"
     t.string "property_type"
     t.string "address"
     t.integer "surface"
