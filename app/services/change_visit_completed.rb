1bd6e9d6626d01ee07ecda8cb26ff01a177b10a5
celine chader <chader.celine@gmail.com>
2023-06-15 11:08:20 +0200

    methods ok + price quote

---
+++ app/services/change_visit_completed.rb
@@ -0,0 +1,25 @@
+class ChangeVisitCompleted
+  def initialize(project: nil)
+    @project = project || Project.order(created_at: :asc).last
+  end
+  def call
+    @project.update(
+      visit_completed: true
+    )
+  end
+end
+
+# ChangeVisitCompleted.new.call
+
+# class ChangeLastProjectVisitDate
+#   def initialize(project: nil)
+#     @project = project || Project.order(created_at: :asc).last
+#   end
+#   def call
+#     @project.update(
+#       visit_date: visit_date + 5.days
+#     )
+#   end
+# end
+
+# ChangeLastProjectVisitDate.new.call
