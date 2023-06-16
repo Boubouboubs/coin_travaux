35721b4fb53261bd7b382a1ca3a362bd63820442
celine chader <chader.celine@gmail.com>
2023-06-16 09:53:37 +0200

    ok markers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -21,20 +21,6 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(this.map)
-    })
-  }
-
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  }
-
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
 
       // Create a HTML element for your custom marker
@@ -47,5 +33,11 @@ export default class extends Controller {
         .setPopup(popup)
         .addTo(this.map)
     })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
